863d7ac9979860f84f224fa2640521ffb4403cd2
SakshamRimal <sakshamo345@gmail.com>
2026-03-10 16:19:52 +0545

    added business logic

--- app/service/user_service.py
+++ app/service/user_service.py
@@ -1,16 +1,29 @@
-from sqlmodel import Session
-
-from app.models.user_models import User
-from app.repository.user_repository import UserRepository
-from app.schemas.user_schemas import UserCreate
-
+from app.repositories.user_repo import UserRepository
+from app.schemas.user_schema import UserOut , UserInLogin , UserWithToken , UserCreate
+from app.secuirty.hashHelper import HashHelper
+from app.secuirty.authHandler import AuthHandler
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
 
 class UserService:
+    def __init__(self , session : Session):
+        self._userRepository = UserRepository(session= session)
 
-    @staticmethod
-    def create_user(session: Session, user_data: UserCreate) -> User:
-        return UserRepository.create_user(session, user_data)
+    def signup(self , user_details: UserCreate) -> UserOut:
+        if self._userRepository.user_exist_by_email(email=user_details.email):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST , detail="Email already registered")
 
-    @staticmethod
-    def get_user(session: Session, user_id: int) -> User:
-        return UserRepository.get_user(session, user_id)
+        hashed_password = HashHelper.get_password_hash(plain_password= user_details.password)
+        user_details.password = hashed_password
+        return self._userRepository.create_user(user_data= user_details)
+
+    def login(self , login_details: UserInLogin) -> UserWithToken:
+        if not self._userRepository.user_exist_by_email(email=login_details.email):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST , detail="Email not found")
+
+        user = self._userRepository.get_user_exist_by_email(email=login_details.email)
+        if HashHelper.verify_password(plain_password= login_details.password, hashed_password= user.password):
+            token = AuthHandler.sign_jwt(user_id=user.id)
+            if token:
+                return UserWithToken(token=token)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST , detail="Incorrect Password")
