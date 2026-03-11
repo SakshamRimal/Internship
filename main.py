5d5fa4c852b1e3dce30aebd3353ed26023226ea4
SakshamRimal <sakshamo345@gmail.com>
2026-03-11 16:54:46 +0545

    added corss origin and routes

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-from app.core.database import  create_db_and_tables
+from app.core.database import create_db_and_tables, seed_default_tiers
 from app.controllers.auth_controller import auth_router
-# execute the code before other task is done we have created a database , session now we have to call the
-#method to table
-# use context manager
+from app.controllers.tier_controller import tier_router
+from app.controllers.wallet_controller import wallet_router
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # initialize DB at start this iss for initializing db at start
     create_db_and_tables()
+    seed_default_tiers()
     yield
 
 app = FastAPI(lifespan=lifespan)
 
-app.include_router(
-    auth_router ,
-    tags=["auth"],
-    prefix="/auth"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
+app.include_router(auth_router, tags=["auth"], prefix="/auth")
+app.include_router(tier_router, tags=["tier"], prefix="/tier")
+app.include_router(wallet_router, tags=["wallet"], prefix="/wallet")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
+    return FileResponse("static/index.html")
