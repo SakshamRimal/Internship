9f36270b771604e87dd90d40d1c2250d798b1ab9
SakshamRimal <sakshamo345@gmail.com>
2026-03-11 16:55:30 +0545

    session class

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -2,7 +2,8 @@ from sqlalchemy.orm import Session
 
 
 class BaseRepository:
-    # no need to rewrite the code for repository it will
+    # no need to rewrite the code for repository it will extend it
+    # here we use the session so that we can intherit it in another class and use it whenever we need
     def __init__(self, session: Session) -> None:
         self.session = session
 
