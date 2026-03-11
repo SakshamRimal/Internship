84ae8d82b386be66a42e2d6cd5dfd70e32ea0ae5
SakshamRimal <sakshamo345@gmail.com>
2026-03-11 16:55:59 +0545

    user connection database

--- app/repositories/user_repo.py
+++ app/repositories/user_repo.py
@@ -2,29 +2,36 @@ from app.repositories.base import BaseRepository
 from app.db.models.user import User
 from app.schemas.user_schema import UserCreate
 
+
 class UserRepository(BaseRepository):
-    def create_user(self , user_data: UserCreate):
-        # user create is the schema for creating the user
-        # since we inherit the repository where there is session dependency
-        # so we inherit it so we don't have to use it again and again
 
+    # here we inherited the base repo because it has session that we need to interact with the database
+    # we have to create the user and keep it into the database
+    def create_user(self, user_data: UserCreate):
+
+        # we extract the data by using model_dump from the database and none will be excluded in this case
         newuser = User(**user_data.model_dump(exclude_none=True))
-        # model dump we dump the data in database store it in database
 
+        # in session, we add new user add the information of the user to the session, and it will add it to the database
         self.session.add(newuser)
-        self.session.commit()
-        self.session.refresh(newuser)
 
+        # after adding the session we have to commit it to the database to save the changes
+        self.session.commit()
+
+        # and refresh the database by passing into the new user
+        self.session.refresh(newuser)
         return newuser
 
-    def user_exist_by_email(self , email: str) -> bool:
-        user = self.session.query(User).filter_by(email=email).first()
-        return bool(user)
+    def user_exist_by_email(self, email: str) -> bool:
 
-    def get_user_exist_by_email(self, email: str) -> User:
+        # use session and query and extract the information by using email and extract the first row it extract only one row that we need
+        user = self.session.query(User).filter_by(email=email).first()
+        return user is not None
+
+    def get_user_by_email(self, email: str) -> User:
         user = self.session.query(User).filter_by(email=email).first()
         return user
 
-    def get_user_by_id(self , user_id: int):
+    def get_user_by_id(self, user_id: int) -> User:
         user = self.session.query(User).filter_by(id=user_id).first()
         return user
