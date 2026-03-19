c608d814ba20292a307709147c933c56ab17d339
SakshamRimal <sakshamo345@gmail.com>
2026-03-19 21:50:46 +0545

    main router config

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from app.controllers import post_controller
+from app.controllers import post_controller, user_controller
 from app.core.db import init_db
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    init_db()   # no await because it is synchronous
-    yield       # application runs here
+    await  init_db()   # no await because it is synchronous
+    yield       # application runs here it pauses the function
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(post_controller.router)
+app.include_router(user_controller.router)
