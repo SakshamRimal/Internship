37b95424432e6eb41bfa38201cab0d6f4d10fa7c
SakshamRimal <sakshamo345@gmail.com>
2026-03-19 21:52:21 +0545

    schemas for validation

--- app/schemas/post_schema.py
+++ app/schemas/post_schema.py
@@ -1,12 +1,35 @@
-from pydantic import BaseModel
+from datetime import datetime
+from pydantic import BaseModel, EmailStr
 from typing import Optional
-import datetime
 
-# ------- this is pydantic schemas for validation -------------------------------------------
-class PostSchemas(BaseModel):
+
+class PostBase(BaseModel):
     title: str
     content: str
     published: bool = True
-    created_at: Optional[datetime.datetime] = None
     rating: Optional[float] = None
+
+class PostCreate(PostBase):
+    pass
+
+# for response schema
+class PostResponse(PostBase):
+    id: int
+    created_at: datetime
+
+    model_config = {
+        "from_attributes": True
+    }
+
+class UserBase(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+
+    model_config = {
+        "from_attributes": True
+    }
 
