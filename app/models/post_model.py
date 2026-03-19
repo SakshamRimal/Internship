d98777f1884ddd79992a5759f848f6de69be950e
SakshamRimal <sakshamo345@gmail.com>
2026-03-19 21:51:49 +0545

    tables of database

--- app/models/post_model.py
+++ app/models/post_model.py
@@ -1,15 +1,14 @@
 from datetime import datetime
 from typing import Optional
-
-from sqlalchemy import Boolean, Column, DateTime, text
+from sqlalchemy import Boolean, Column, DateTime, text, String
 from sqlmodel import Field, SQLModel
 
 class Posts(SQLModel, table=True):
     __tablename__ = "posts"
 
     id: Optional[int] = Field(default=None, primary_key=True)
-    title: str = Field(max_length=500)
-    content: str = Field(max_length=500)
+    title: str = Field(max_length=500 , sa_column=Column(String(500)))
+    content: str = Field(max_length=500 , sa_column=Column(String(500)))
     published: bool = Field(default=True, sa_column=Column(Boolean, server_default=text("1")))
     created_at: datetime = Field(
         default_factory=datetime.utcnow,
@@ -17,4 +16,16 @@ class Posts(SQLModel, table=True):
     )
     rating: Optional[float] = Field(default=None, nullable=True)
 
+# sql model will automatically create the table based on the name of class, but we can set table name too
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    email: str = Field(max_length=100, sa_column=Column(String(100), nullable=False, unique=True))
+    password: str = Field(max_length=100, sa_column=Column(String(100), nullable=False))
+    created_at: datetime = Field(
+        default_factory=datetime.utcnow,
+        sa_column=Column(DateTime, server_default=text("CURRENT_TIMESTAMP")),
+    )
 
