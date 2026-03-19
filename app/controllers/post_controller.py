47510a3a57fc4692e1ceb8c9f550dbca1c4c01c9
SakshamRimal <sakshamo345@gmail.com>
2026-03-19 21:51:06 +0545

    routing reponse model

--- app/controllers/post_controller.py
+++ app/controllers/post_controller.py
@@ -1,32 +1,36 @@
-from fastapi import APIRouter, Depends
+from typing import List
+from fastapi import APIRouter, status
 from app.core.db import SessionDep
-from app.schemas.post_schema import PostSchemas
-from app.service.post_service import get_all_post_service, create_post_service, get_post_by_id_service, \
-    update_post_service, delete_post_service
+from app.schemas.post_schema import PostCreate , PostResponse  # use the correct schema
+from app.service.post_service import post_service
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/posts",
+    tags=["posts"],
+)
 
-@router.get("/posts")
+@router.get("" , response_model=List[PostResponse])
 async def get_posts_controller(session: SessionDep):
-    posts = await get_all_post_service(session)
-    return {"data": posts}
+    posts = await post_service.get_all_post_service(session)
+    return posts
 
-@router.post("/posts")
-async def create_post_controller(post: PostSchemas, session: SessionDep):
-    new_post = await create_post_service(session , post)
-    return {"data": new_post}
+@router.post("" , status_code=status.HTTP_201_CREATED , response_model=PostResponse)
+async def create_post_controller(post: PostCreate, session: SessionDep):
+    new_post = await post_service.create_post_service(session, post)
+    return new_post
 
-@router.get("/posts/{post_id}")
+@router.get("/{post_id}" , response_model=PostResponse)
 async def get_post_by_id_controller(post_id: int, session: SessionDep):
-    post = await get_post_by_id_service(session, post_id)
-    return {"data": post}
+    post = await post_service.get_post_by_id_service(session, post_id)
+    return post
 
-@router.put("/posts/{post_id}")
-async def update_post_controller(post_id: int, post: PostSchemas, session: SessionDep ):
-    updated_post = await update_post_service(session, post_id, post)
-    return {"data": updated_post}
+@router.put("/{post_id}" , response_model=PostResponse)
+async def update_post_controller(post_id: int, post: PostCreate, session: SessionDep):
+    updated_post = await post_service.update_post_service(session, post_id, post)
+    return updated_post
 
-@router.delete("/posts/{post_id}")
-async def delete_post_controller(post_id: int, session: SessionDep ):
-    deleted = await delete_post_service(session, post_id)
-    return {"data": deleted}
+@router.delete("/{post_id}")
+async def delete_post_controller(post_id: int, session: SessionDep):
+    deleted = await post_service.delete_post_service(session, post_id)
+    return deleted
+
---
+++ app/controllers/user_controller.py
@@ -0,0 +1,25 @@
+from typing import List
+
+from fastapi import APIRouter
+from app.service.user_service import user_service
+from app.core.db import SessionDep
+from app.schemas.post_schema import UserResponse , UserBase
+router = APIRouter(
+    prefix="/user",
+    tags=["users"],
+)
+
+@router.get("" ,response_model=List[UserResponse])
+async def get_users(session: SessionDep):
+    users = await user_service.get_user_service(session)
+    return users
+
+@router.post("" , response_model=UserResponse)
+async def create_user(session: SessionDep , user_data: UserBase):
+    users = await user_service.create_user_service(session , user_data)
+    return users
+
+@router.get("/{user_id}" , response_model=UserResponse)
+async def get_user_by_id(session: SessionDep , user_id: int):
+    user = await user_service.get_user_by_id(session , user_id)
+    return user
